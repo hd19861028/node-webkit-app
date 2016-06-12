51b9e71a457e123a9cb54fee8dd38af8f2ecfdd6
marshu <282642547@qq.com>
2016-06-12 22:10:46 +0800

    asd

--- nanpin/js/page3.js
+++ nanpin/js/page3.js
@@ -2,48 +2,16 @@ var ang = new angular();
 var info = {};
 var newsType = 0;
 var pageIndex = 1;
-var pageTotal = 0;
+var ids = [];
+var total = 0;
+var current = 0;
 var maxIndex = 0;
+var timer = null;
+var loopIndex = 0;
+var loopTotal = 1;
 
 var search = WebStorage.Session.Get('search');
 var json = JSON.parse(search);
-
-function BindEvent() {
-	if (info.total == 0) {
-		$('.list').html('<p class="nodata">没有数据！</p>')
-	} else {
-		pageTotal = info.total;
-		maxIndex = ~~(pageTotal / 20);
-		if (pageTotal % 20 != 0) {
-			maxIndex += 1;
-		}
-	}
-	swipt('list',
-		function() {
-			NewsList(true)
-		},
-		function() {
-			NewsList(false)
-		})
-	$('.list li').each(function(index) {
-		$(this).click(function() {
-			var id = $(this).attr('id');
-			Ajax(server + 'api/news/detail', {
-				id: id
-			}, 'get', 'json', function(r) {
-				var img = server + 'api/news/image/' + r.image;
-				$('.logo').attr('src', img);
-				$('.title span').html(r.title);
-				$('.title small').html(r.updatetime);
-				$('.content').html(r.content + '<p class="footer">' + r.footer + '</p>');
-				$('.model').show();
-			})
-		})
-	})
-	$('.delete').click(function() {
-		$('.model').hide();
-	})
-}
 
 function NewsList(isnext) {
 	var isload = false;
@@ -62,13 +30,73 @@ function NewsList(isnext) {
 		Ajax(server + 'api/news', {
 			id: newsType,
 			index: maxIndex == 0 ? 1 : pageIndex,
-			size: 20
+			size: 2000
 		}, 'get', 'json', function(r) {
 			info.rows = r.rows;
 			info.total = r.total;
-			ang.set('info', info, BindEvent)
+			for (var i = 0; i < info.rows.length; i++) {
+				ids.push(info.rows[i].id);
+			}
+			total = r.total;
+			ShowItem(current)
+			swipt('list',
+				function() {
+					if (current < total) {
+						current += 1;
+						ShowItem(current)
+					}
+				},
+				function() {
+					if (current > 0) {
+						current -= 1;
+						ShowItem(current)
+					}
+				})
 		})
 	}
+}
+
+function ShowItem(index) {
+	var id = ids[index];
+	Ajax(server + 'api/news/detail', {
+		id: id
+	}, 'get', 'json', function(r) {
+		var isNeedLoop = false;
+		var img = server + 'api/news/image/' + r.image;
+		var i1 = $('.logo').get(0);
+		$(i1).attr('src', img);
+		if (r.image1) {
+			var img1 = server + 'api/news/image/' + r.image1;
+			var i2 = $('.logo').get(1);
+			$(i2).attr('src', img1);
+			isNeedLoop = true;
+			loopTotal += 1;
+		}
+		if (r.image2) {
+			var img2 = server + 'api/news/image/' + r.image2;
+			var i3 = $('.logo').get(2);
+			$(i3).attr('src', img2);
+			isNeedLoop = true;
+			loopTotal += 1;
+		}
+		$('.title span').html(r.title);
+		$('.title small').html(r.updatetime);
+		$('.content').html(r.content + '<p class="footer">' + r.footer + '</p>');
+		if (isNeedLoop) {
+			timer = setInterval(function() {
+				if (loopIndex < loopTotal - 1) {
+					loopIndex += 1;
+				} else {
+					loopIndex = 0;
+				}
+				$('.logo').hide();
+				var showLog = $('.logo').get(loopIndex);
+				$(showLog).show()
+			}, 5000)
+		}
+		$('.model').show();
+	})
+	current = index;
 }
 
 Ajax(server + 'api/menu', {
@@ -89,5 +117,8 @@ $('.last').click(function() {
 })
 
 $('body').click(function() {
-	NewsList(true);
+	if (current < total - 1) {
+		current += 1;
+		ShowItem(current)
+	}
 })
